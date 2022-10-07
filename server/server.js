edd65004637207c2897a854b71303a76989a7cd4
rfabreu <raabreugomes@gmail.com>
2022-10-07 10:28:10 -0400

    Minor refactor update

--- server/server.js
+++ server/server.js
@@ -10,3 +10,8 @@ app.use(express.json());
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
+
+
+// Credits: Parts of this codebase may reference the lesson materials of the University of Toronto coding Bootcamp
+// Tutor Sara Neves Pereira from the University of Toronto coding Bootcamp was an integral part in assisting during development.
